0a64dabde5de928556657e542e851dbf0b9835a2
Sivadhanushi <123049453+Sivadhanushi@users.noreply.github.com>
2023-02-09 18:49:34 +0530

    Add files via upload

---
+++ Bank_3.java
@@ -0,0 +1,19 @@
+package zohoassessmentfeb;
+
+import java.util.Scanner;
+
+public class Bank_3 {
+
+	public static void main(String[] args) {
+		    Scanner in = new Scanner(System.in);
+		    System.out.println("Enter N :");
+		    int n = in.nextInt();
+		    System.out.println("Trailing Days :");
+		    int d = in.nextInt();
+		    System.out.println("Enter Elements :");
+           int [] expenditure =new int[n];
+           
+           
+	}
+
+}
--- RotateMatrix_6.java
+++ RotateMatrix_6.java
@@ -3,21 +3,45 @@ package zohoassessmentfeb;
 public class RotateMatrix_6 {
 
 	public static void main(String[] args) {
-		int a[][]=  {{1,2,3},
-			           { 4,5,6},
-		               {7,8,9}
-		              };
-		int rowstart=0;int rowEnd=0;
-		int columnStart=0;int columnEnd=0;
-		   for(int i=0;i<columnStart;i++) {
-			   System.out.println(a[columnStart][i]);}
-			   
-		   
-	       for(int j=0;j<rowstart;j++) {
-			   System.out.println(a[columnStart][j]);}
+		int num=0;
+		int[][] a = new int[num][num];
+		int rowstart = 0, rowend = a.length;
+		int colstart = 0, colend = a[0].length ;
+
+		//int a = 1;
+		while (rowstart < rowend && colstart < colend) {
+
+			for (int i = colstart; i < colend; i++) {
+				System.out.println(a[rowstart][i] +" ");
+			}
+			rowstart+=1;
+			for (int i = rowstart; i <= rowend; i++) {
+				System.out.println(a[i][colend-1]+ " " );
+			}
+			colend-=1;
+			for (int i = colend; i >= colstart; i--) {
+				System.out.println(a[rowend][i]) ;
+			}
+			rowend-=1;
+			for (int i = rowend-1; i >= rowstart; i--) {
+				System.out.println(a[i][colstart]);
+			}
+			colstart += 1;
+			//rowstart++;
+			//colend--;
+			//rowend--;
+		}
+
+		/*for (int arr[] : array) {
+			for (int i : arr) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}*/
+	}
 
 		 }
-}		 	 
+	 	 
 		 
 
 
